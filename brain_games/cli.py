22c97ed70c66bfc8ecb867cd4d220a4d55d553b8
BasedOnEvidence <testtypotest@gmail.com>
2020-11-06 23:04:15 +0300

    Fix lint errors

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,14 @@
 import prompt
 
+
 def welcome_user():
-	name = prompt.string('May I have your name? ')
-	print("Hello, {}!".format(name))
+    name = prompt.string('May I have your name? ')
+    print("Hello, {}!".format(name))
+
 
 def main():
-	welcome_user()
+    welcome_user()
+
 
 if __name__ == "__main__":
-	main()
+    main()
