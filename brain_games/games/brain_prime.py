c7ec2b42d77b965505b709f951e27dcca8eccd8f
BasedOnEvidence <testtypotest@gmail.com>
2020-12-16 21:04:58 +0300

    Optimize brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -3,15 +3,9 @@ import random
 GAME_WELCOME_STR = "Answer \"yes\" if given number " \
                    "is prime. Otherwise answer \"no\"."
 
-START_NUM = 0
-END_NUM = 100
-
-
-def odd_num_generator(start_num, end_num):
-    result = random.randint(start_num, end_num)
-    if result % 2 == 0:
-        result = result + 1
-    return result
+START_NUM = 1
+END_NUM = 101
+STEP = 2
 
 
 def is_prime(num):
@@ -26,7 +20,7 @@ def is_prime(num):
 
 
 def generate_task():
-    task_number = odd_num_generator(START_NUM, END_NUM)
+    task_number = random.randrange(START_NUM, END_NUM, STEP)
     correct_answer = "no"
     if is_prime(task_number):
         correct_answer = "yes"
